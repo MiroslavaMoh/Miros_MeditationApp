a83405b7406294bf3f66a7df528addb90a0af45e
MiroslavaMoh <miroslavamoh252@gmail.com>
2025-03-06 22:37:59 -0600

    RoundButton-Vid2-31.07

--- meditationapp/lib/common_widget/round_text_field.dart
+++ meditationapp/lib/common_widget/round_text_field.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:meditationapp/common/color_extension.dart';
+
+//meditationapp\lib\common\color_extension.dart
 
 class RoundTextField extends StatelessWidget {
   final String hintText; //Miros- tipo de variable - tipo de dato - nombre de variable
@@ -14,6 +17,33 @@ class RoundTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height:60,
+      margin:const EdgeInsets.symmetric(horizontal:20),
+      padding: const EdgeInsets.only(top: 8),
+      decoration: BoxDecoration(
+        color: TColor.txtBG,
+        borderRadius: BorderRadius.circular(15)
+        
+        ),
+        child: TextField(  
+          controller: controller, //instancia que actualiza el campo 
+          keyboardType:keyboardType, //Tipo de daros recibidos
+          obscureText:obscureText ?? false, //Tipo Bool para esconder contraseñas, en esta caso esta desactivado
+          style: TextStyle(
+            color: TColor.primaryText,
+            fontSize: 16
+          ),
+          decoration: InputDecoration(
+            contentPadding:const EdgeInsets.symmetric(horizontal: 20),
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            hintText: hintText,
+            suffixIcon: right,
+            hintStyle: TextStyle(
+              color:TColor.secondaryText, 
+              fontSize: 16
+              ),
+          )
+          ),
     );
   }
 }
--- meditationapp/lib/screen/login/startup_screen.dart
+++ meditationapp/lib/screen/login/startup_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meditationapp/common/color_extension.dart';
+
+//Página de inicio
 
 class StartUpScreen extends StatefulWidget {
   const StartUpScreen({super.key});
@@ -8,10 +12,58 @@ class StartUpScreen extends StatefulWidget {
 }
 
 class  _StartUpScreenState extends State <StartUpScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "meditationapp/android/assets/img/starup_top.png", 
+            width: double.maxFinite, 
+            fit: BoxFit.fitWidth,
+            ),
+            const Spacer(),
 
+            const Spacer(),
+
+              Row(
+                children: [
+                  Text("¿Ya tienes una cuenta?",
+                  style: TextStyle( 
+                    color:TColor.secondaryText,
+                    fontSize:14, 
+                    fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  TextButton(onPressed: (){
+
+                  },child: Text(
+                    "Inicr seción",
+                    style: TextStyle( 
+                    color:TColor.secondaryText,
+                    fontSize:14, 
+                    fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  ),
+                ],
+              ),
+
+            const Spacer(),
+
+
+          ],
+      )
     );
   }
 }

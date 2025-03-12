aa8b0010dcc5516679389071655d487e39aac047
MiroslavaMoh <miroslavamoh252@gmail.com>
2025-03-12 07:44:44 -0600

    SignUpScreen-Vid2-Terminado

--- meditationapp/lib/screen/login/sign_up_screen.dart
+++ meditationapp/lib/screen/login/sign_up_screen.dart
@@ -3,24 +3,21 @@ import 'package:flutter/services.dart';
 import 'package:meditationapp/common/color_extension.dart';
 import 'package:meditationapp/common_widget/round_button.dart';
 import 'package:meditationapp/common_widget/round_text_field.dart';
-import 'package:meditationapp/screen/login/sign_up_screen.dart';
+//import 'package:meditationapp/screen/login/startup_screen.dart';
 
 //Página de inicio
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen ({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen ({super.key});
 
   @override
-  State <LoginScreen> createState() =>  _LoginScreenState();
+  State <SignUpScreen> createState() =>  _SignUpScreenState();
 }
 
-class  _LoginScreenState extends State <LoginScreen> {
+class  _SignUpScreenState extends State <SignUpScreen> {
 
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
-  }
+  bool isTrue = false; //Checkbox de privadidad falso por default
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,7 +60,7 @@ class  _LoginScreenState extends State <LoginScreen> {
 
                         const SizedBox(height:25,),
 
-                        Text("Bienvenido de regreso",
+                        Text("Crea tu cuenta",
                           style: TextStyle( 
                             color:TColor.primaryText,
                             fontSize:28, 
@@ -165,72 +162,62 @@ class  _LoginScreenState extends State <LoginScreen> {
                       ),
 
                 const SizedBox(height:35),
+                RoundTextField(hintText:"Nombre de usuario"),
+                const SizedBox(height:35),
                 RoundTextField(hintText:"Correo electronico"),
                 const SizedBox(height:20),
                 RoundTextField(hintText:"Contraseña",obscureText: true,),
                 const SizedBox(height:20),
 
+                Padding(
+                   padding:const EdgeInsets.symmetric(horizontal:20),
+                  child: Row(
+                      children: [
+                        Text(
+                          "He leido el",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: TColor.secondaryText,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            ),
+                          ),
+
+                         Text(
+                          " aviso de privacidad",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: TColor.primary,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        const Spacer(),
+                        IconButton(onPressed: (){
+                          setState((){
+                            isTrue =!isTrue;
+                          });
+                        },
+                        icon: Icon(
+                        isTrue
+                            ? Icons.check_box
+                            : Icons.check_box_outline_blank_rounded,
+                        color: isTrue ? TColor.primary : TColor.secondaryText,
+                        ),
+                        ),
+                      ],                   
+                   ),
+                ),
+                
+                const SizedBox(height:20),
 
                 //BTN-Registrarme morado
+                
                 RoundButton( //BTN de Common wodgets, recordar importar archivo round_button.dart
-                  title:"Iniciar Sesión",
+                  title:"Registrarme",
                   onPressed:(){}
                 ),
 
-                //Texto de Iniciar sesión, dos colores 2 elementos
-                 
-                    TextButton(
-                    onPressed: () {
-                      
-                    },
-                    child: Text(
-                      "¿Olvidaste tu contraseña?",
-                      style: TextStyle(
-                        color: TColor.primaryText,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                      
-                      
-
-                      const Spacer(),
-                          
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Crear nueva cuenta",
-                          textAlign: TextAlign.center,
-                          style: TextStyle( 
-                            color:TColor.secondaryText,
-                            fontSize:14, 
-                            fontWeight: FontWeight.w600,
-                            ),
-                          ),
-
-                          //Conectar el boton para que te envie a login_screen
-                          TextButton(onPressed: (){
-                            context.push(const SignUpScreen());
-                          },
-
-                          //const Spacer(),
-
-                          child: Text(
-                            "Registrate",
-                            style: TextStyle( 
-                            color:TColor.primary,
-                            fontSize:14, 
-                            fontWeight: FontWeight.w600,
-                            ),
-                          ),
-
-                          ),
-                        ],
-                      ),
-
-                    
                 const Spacer(),
               ],
           ),
